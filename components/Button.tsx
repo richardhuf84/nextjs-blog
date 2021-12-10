13ba4d7b651cd502b40b87efd6e93764d7da3ac9
Richard Huf <richardhuf84@gmail.com>
2021-12-10 19:14:50 +1100

    feat: add styled components and basic button

---
+++ components/Button.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const Button = styled.button`
+  background: "#48C9B0",
+  appearance: "none",
+  color: "white",
+  border: "0",
+  padding: "10px 14px",
+  border-radius: "8px",
+  font-weight: "700"
+
+`;
